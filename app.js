3def050a1f40a767e748b3c97a4438172589336e
hyechan90 <hyechan90@naver.com>
2019-11-22 08:08:10 +0900

    Cookies

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const io = require('socket.io')(3000);
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+app.use(cookieParser());
 app.use(express.static('DB'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,6 +22,22 @@ app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/public/login.html');
 });
 
+app.post('/login', (req, res) => {
+  const input = {
+    name: req.body.name,
+    password: req.body.password
+  };
+  const user = new Users({ input });
+  user.findOne({ name: input.name }, (err, result) => {
+    if (err) throw err;
+    if (!result) {
+      console.log('아이디가 잘못되었습니다.');
+      alert('아이디가 잘못되었습니다.');
+    } else {
+    }
+  });
+});
+
 app.get('/register', (req, res) => {
   res.sendFile(__dirname + '/public/register.html');
 });
@@ -27,14 +45,16 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 10);
-    const user = new Users({
+    const input = {
       name: req.body.name,
       email: req.body.email,
       password: hash
-    });
+    };
+    const user = new Users({ input });
     user.save((err, result) => {
       if (err) throw err;
       console.log(result);
+      res.redirect('/login');
     });
   } catch (err) {
     console.error('회원가입 실패', err);
